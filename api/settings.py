626ad61d63a3b791db4eefc817ae21a0b1d684b9
DanielDante15 <danieldantefm@gmail.com>
2024-01-15 13:52:53 -0300

    commit

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = os.environ.get ("DEBUG", True)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
